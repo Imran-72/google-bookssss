ba8fabcefd8b5106df8d537e664bfa712d3ad518
Imran Abdurakhmanov <petrow318@gmail.com>
2021-12-06 15:45:50 +0300

    сделал компонент header

---
+++ src/app/components/header/header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <div>Logo</div>
+          <div>
+            <input />
+            <button>поиск</button>
+          </div>
+          <div>вход/регистрация</div>
+          <div>корзина</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
